b67e2babff3702f15111678eef585c3ee17ba56f
sun <annjin12@naver.com>
2023-05-03 23:07:29 +0900

    Second Root (N이 K의 배수인 가정 하에 정상작동)

--- Greedy/justOne.py
+++ Greedy/justOne.py
@@ -15,3 +15,23 @@ def problem1():
         n -= 1
         result += 1
     print(result)
+    
+def problem2():
+    n, k = map(int, input().split())
+    result = 0
+  
+    while True:
+        # n이 k의 배수일 경우에만 적용됨
+        target = (n // k) * k
+        result += n - target
+        
+        n = target
+        if n < k:
+            break
+        result += 1
+        n //= k
+        
+    result += (n - 1)
+    print(result)
+    
+problem2()
