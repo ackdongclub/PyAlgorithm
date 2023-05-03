cfdd9709122186f2e3a6aecb4896c603300b7340
sun <annjin12@naver.com>
2023-05-03 23:58:04 +0900

    comment

--- Memory/TBLR.py
+++ Memory/TBLR.py
@@ -7,12 +7,12 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 move_types = ['L', 'R', 'U', 'D']
 
-for plan in plans:
-    for i in range(len(move_types)):
+for plan in plans: # 입력 받는 값을 차례대로 넣어준다
+    for i in range(len(move_types)): 
         if plan == move_types[i]:
             nx = x + dx[i]
             ny = y + dy[i]
-        if nx < 1 or ny < 1 or nx > n or ny >n:
+        if nx < 1 or ny < 1 or nx > n or ny > n: # 행을 벗어나면 무시됨
             continue
         x, y = nx, ny
 print(x, y)
