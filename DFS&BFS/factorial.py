34c80d30be647503d3d69bcdade395f3a63e18fc
sun <annjin12@naver.com>
2023-05-22 20:20:45 +0900

    인접 정렬 방식

--- DFS&BFS/factorial.py
+++ DFS&BFS/factorial.py
@@ -10,5 +10,5 @@ def factorial_recursive(n):
     if n <= 1: 
         return 1 # n이 1보다 작거나 같으면 1을 반환
     return n * factorial_recursive(n-1) # n - 1을 반복하며 n이 1이 되면 1을 반환해서 멈춤
-print('반복적으로 구현: ', factorial_iterative(5))
-print('반복적으로 구현: ', factorial_recursive(5))
+print('반복적으로 구현:', factorial_iterative(5)) # 120
+print('반복적으로 구현:', factorial_recursive(5)) # 120
