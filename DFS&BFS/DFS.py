50f3cc397c662f592fe6cbf397d77230a50e7086
sun <annjin12@naver.com>
2023-05-22 21:06:18 +0900

    DFS 알고리즘

--- DFS&BFS/DFS.py
+++ DFS&BFS/DFS.py
@@ -22,3 +22,29 @@ graph[1].append((0, 7))
 graph[2].append((0, 5))
 
 print(graph)
+
+# DFS Algorithm
+def dfs(graph, v, visited):
+    visited[v] = True # 현재 방문한 노드
+    print(v, end='')
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+    
+    # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    []
+    , [2, 3, 8]
+    , [1, 7]
+    , [1, 4, 5]
+    , [3, 5]
+    , [3, 4]
+    , [7]
+    , [2, 6, 8]
+    , [1, 7]
+]
+    
+visited = [False] * 9
+    
+dfs(graph, 1, visited)
