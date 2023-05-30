efcfb60d71de3673e723c6e9e024a1fa2ff8785b
sun <annjin12@naver.com>
2023-05-30 23:51:53 +0900

    스와프

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -6,5 +6,5 @@ for i in range(len(array)):
         if array[min_index] > array[j]:
             min_index = j # 가장 작은 숫자의 위치 보다 j가 더 작으면 가장 작은 인덱스의 위치가 변경된다
             
-    array[i], array[min_index] = array[min_index], array[i] # 스와프
+    array[i], array[min_index] = array[min_index], array[i] # 스와프: 특정 리스트가 주어졌을 때 두 변수의 우치를 변경하는 작업
 print(array)
