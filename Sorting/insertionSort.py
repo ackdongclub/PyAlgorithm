fbbe34a625cc8696be6333dd52e65a81935fd6ce
sun <annjin12@naver.com>
2023-06-04 03:24:12 +0900

    range의 매개변수

--- Sorting/insertionSort.py
+++ Sorting/insertionSort.py
@@ -1,6 +1,6 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1, len(array)):
+for i in range(1, len(array)): # range(start, end, step)
     for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하면서 반복
         if array[j] < array[j - 1]: # 자리 이동
             array[j], array[j - 1] = array[j - 1], array[j]
