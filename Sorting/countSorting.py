410fbc7462b2050a8beddefad6d80fdde8b43ebe
sun <annjin12@naver.com>
2023-06-08 23:45:32 +0900

    수정

--- Sorting/countSorting.py
+++ Sorting/countSorting.py
@@ -5,6 +5,6 @@ count = [0] * (max(array) + 1) # 모든 값을 0으로 초기화 index 0 ~ n + 1
 for i in range(len(array)):
     count[array[i]] += 1 # 각 데이터에 해당하는 인덱스 값 증가
     
-for i in range(count[i]): # 리스트에 기록된 정렬 정보 확인
+for i in range(count): # 리스트에 기록된 정렬 정보 확인
     for j in range(count[i]):
         print(i, end=' ') # 등장한 횟수만큼 인덱스 출력
