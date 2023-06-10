68f0a1f785ea91ded47e065285691e8ad27f4904
sun <annjin12@naver.com>
2023-06-10 23:59:32 +0900

    스스로 풀어보기

--- Sorting/test/upToDown.py
+++ Sorting/test/upToDown.py
@@ -1,10 +1,11 @@
-n = int(input())
+n = int(input()) # N값 입력 받음
 
-array = []
-for i in range(n):
-    array.append(int(input()))
+array = [] # 배열 생성
+
+for i in range(n): # N만큼 돈다
+    array.append(int(input())) # N개 만큼 array에 값을 넣어준다
     
-array = sorted(array, reverse=True)
+result = sorted(array, reverse=True)
 
 for i in array:
     print(i, end=' ')
