47bb3bd72dce5e52af068b60bb1464530d1b8ea3
sun <annjin12@naver.com>
2023-06-10 22:54:27 +0900

    hoare sort2 복습

--- Sorting/quickSorting2.py
+++ Sorting/quickSorting2.py
@@ -13,5 +13,20 @@ def quick_sort(array):
     
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
-print(quick_sort(array))
+# print(quick_sort(array))
     
+# 호어정렬2 복습 - 반복을 더 많이 하므로 비효율적이지만 더욱 직관적인 코딩
+def hoare_sort_2(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+    
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    print(hoare_sort_2(left_side) + [pivot] + hoare_sort_2(right_side)) # 재귀호출로 분할했던 왼쪽 정렬
+    return hoare_sort_2(left_side) + [pivot] + hoare_sort_2(right_side) # 재귀호출로 분할했던 오른쪽 정렬
+print(hoare_sort_2(array))
+        
